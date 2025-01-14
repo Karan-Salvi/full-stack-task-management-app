04e70104d0723fce3b8961271ee4ef6d772297bd
Karan <karansalviwork@gmail.com>
2025-01-14 18:09:14 +0530

    feat: initialized project with dependencies and setup routing

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import AuthLayout from "./pages/Auth/AuthLayout";
+import Login from "./pages/Auth/Login";
+import Signup from "./pages/Auth/Signup";
+import Menu from "./pages/MenuPage/Menu";
+import Cart from "./pages/Cart/Cart";
+import Order from "./pages/OrderPage/Order";
+import OrderHistory from "./pages/OrderPage/OrderHistory";
+import Navbar from "./components/Navbar";
+import NotFound from "./pages/NotFound/NotFound";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<AuthLayout />}>
+          <Route path="login" element={<Login />} />
+          <Route path="signup" element={<Signup />} />
+        </Route>
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/order" element={<Order />} />
+        <Route path="/order-history" element={<OrderHistory />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ Frontend/src/pages/OrderPage/Order.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Order = () => {
+  return <div>Orders Page</div>;
+};
+
+export default Order;
